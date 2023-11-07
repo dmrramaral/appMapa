3cc9471dd9b26ab39d2178e3c0e49e37d642f5e9
Amaral <dmrramaral@gmail.com>
2023-11-07 14:03:46 -0300

    Style: Formatação UserModel

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    
-    email: {type: String, unique: true , required: true},
-    
-    password: {type: String, required: true},
+    name: { type: String, required: true },
 
-    token: {type: String, required: false},
+    email: { type: String, unique: true, required: true },
+
+    password: { type: String, required: true },
+
+    createdAt: { type: Date, default: Date.now },
+
+    token: { type: String, required: false },
+
+
+
 });
 
 const User = mongoose.model("user", UserSchema);
